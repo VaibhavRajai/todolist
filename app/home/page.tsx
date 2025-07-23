8600c39bc6ac41c8cb58fb853e562a27cd65fae0
VaibhavRajai <rajaivaibhav28@gmail.com>
2025-07-23 18:37:37 +0530

    final push

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -359,14 +359,15 @@ const TaskForm: React.FC = () => {
           className="flex-1 px-4 text-gray-700 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
           required
         />
-        <input
-          type="date"
-          value={taskDate}
-          onChange={(e) => setTaskDate(e.target.value)}
-          min={new Date().toISOString().split("T")[0]} 
-          className="flex-1 px-4 text-gray-700 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
-          required
-        />
+<input
+  type="date"
+  value={taskDate}
+  onChange={(e) => setTaskDate(e.target.value)}
+  min={new Date().toISOString().split("T")[0]}
+  className="flex-1 px-4 py-2 text-black bg-white border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
+  required
+/>
+
         <button
           type="submit"
           className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors font-medium"
